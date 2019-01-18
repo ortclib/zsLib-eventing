e4ca18eebaa9ec7e09c9cf3a1ecbf5632a757f7e
Sergej Jovanovic <sergej@gnedo.com>
2019-01-18 20:43:54 +0100

    Updated python scripts that run idl and event compilations. Update event compilation templates.

--- runIDLCompiler.py
+++ runIDLCompiler.py
@@ -10,7 +10,6 @@ idlAlreadyCompletedFlagFile=inputArray[2]
 toolchainCPU=inputArray[3]
 idlOutputPath=inputArray[4]
 idlLanguages=inputArray[5]
-#tempToolchain=toolchain.split(":")
 hostCPU=inputArray[6]
 
 def getCompilerPath():
@@ -37,37 +36,25 @@ currentWorkingPath=os.getcwd()
 pathname = os.path.dirname(sys.argv[0]) 
 
 idlCompilationFlagPath=dir_path + "/" + idlAlreadyCompletedFlagFile
-print "runIDLCompiler - idlCompilationFPath: " + idlCompilationFlagPath
+
 if not os.path.isfile(idlCompilationFlagPath):
-  print("Running idl compilation")
+  print("Running idls compilation...")
 
   compilerFullPath = getCompilerPath()
   if (compilerFullPath==None):
     sys.exit("Idl compiler doesn't exist")
     
-  #compilerNewPath = os.getcwd() + "/" + compilerPath;
-  #if not os.path.isfile(compilerNewPath):
-  #  compilerNewPath = os.getcwd() + "/" + hostCPU + "/" + compilerPath;
-  #  if not os.path.isfile(compilerNewPath):
-  #    sys.exit("Idl compiler doesn't exist")
-      
   os.chdir(os.path.dirname(idlPath))
   jsonFile=os.path.basename(idlPath)
 
-  commandPath = compilerFullPath + " -idl " + idlLanguages + " -c " + jsonFile + " -o " + idlOutputPath
+  commandToExecute = compilerFullPath + " -idl " + idlLanguages + " -c " + jsonFile + " -o " + idlOutputPath
 
-  print "runIDLCompiler - idlPath: " + idlPath
-  print "runIDLCompiler - jsonFile: " + jsonFile
-  print "runIDLCompiler - idlAlreadyCompletedFlagFile: " + idlAlreadyCompletedFlagFile
-  print "runIDLCompiler - NewWorkingPath:" + os.getcwd()
-  print "runIDLCompiler - compilerFullPath: " + compilerFullPath
-  print "runIDLCompiler - command: " + commandPath
-
-  result=os.system(commandPath)
-  if (result!=0):
-    sys.exit("Failed idl compilation" + str(result))
+  result = os.system(commandToExecute)
+  
+  if result == 0:
+    open(idlCompilationFlagPath,'w').close()
+  else:
+    sys.exit("Failed idls compilation with error code " + str(result))
     
-  open(idlCompilationFlagPath,'w').close()
-  os.chdir(os.path.dirname(currentWorkingPath))
 else:
   print("Idls have been already compiled")
